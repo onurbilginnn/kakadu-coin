58d2bccd5f5a5d5b61deb48ec0c842b16d86c876
onurbilgin-jll <onur.bilgin@jll.com>
2026-03-11 11:44:05 +0300

    fixed assets

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,14 @@
 import type { NextConfig } from "next";
 
+const repoName = "kakadu-coin";
+
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   output: "export",
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
